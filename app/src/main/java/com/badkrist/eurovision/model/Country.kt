cb9106d65c4831fbc6b4638246675dfb5003d4ad
baptister <baptiste.rendu@noteo.info>
2020-04-26 10:17:04 +0200

    Revert "first commit"
    This reverts commit 5bbef7f286d3bd0f1f9f7ee2c3567e94792443ab.

--- app/src/main/java/com/badkrist/eurovision/model/Country.kt
+++
@@ -1,8 +0,0 @@
-package com.badkrist.eurovision.model
-
-import com.squareup.moshi.Json
-
-data class Country(
-    @field:Json(name = "flag") val flag: String? = "",
-    @field:Json(name = "name") val name: String? = ""
-)
--- app/src/main/java/com/badkrist/eurovision/model/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.badkrist.eurovision.model
-
-data class Song(
-    var countryCode: String? = "",
-    var img: String? = "",
-    var link: String? = "",
-    var number: Int? = 0,
-    var title: String? = ""
-)
